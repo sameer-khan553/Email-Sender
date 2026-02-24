f0f6957be0bcd94680d51007f97ef89b95b1c850
Sameer Khan <155846059+sameer-khan553@users.noreply.github.com>
2026-02-24 17:46:22 +0530

    updated with frontend

--- src/main/java/com/example/otp_Sender/MailRequest.java
+++ src/main/java/com/example/otp_Sender/MailRequest.java
@@ -5,7 +5,6 @@ import lombok.Data;
 
 @Data
 public class MailRequest {
-
     private String to;
     private String subject;
     private String text;
--- src/main/java/com/example/otp_Sender/mailSenderApplication.java
+++ src/main/java/com/example/otp_Sender/mailSenderApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class mailSenderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(mailSenderApplication.class, args);
+
+        SpringApplication.run(mailSenderApplication.class, args);
 	}
 
 }
